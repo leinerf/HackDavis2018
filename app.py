0da44ee4764ba38f4672613b5fa1b07ad2930eaa
Freniel Zabala <frenielzabala@gmail.com>
2018-01-20 16:40:54 -0800

    added more routes

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
+     return redirect(url_for('home'))
+
+
+@app.route('/home', methods=['GET'])
+def home():
     return render_template("home.html")
+
+
+@app.route('/students/<students>', methods=['GET'])
+def student():
+    return "User %s" % username
+
 
 
 if __name__ == '__main__':
