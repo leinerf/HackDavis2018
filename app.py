9c311b479d7c6b3b6efc66bfcb18f757f160228e
Freniel Zabala <frenielzabala@gmail.com>
2018-01-20 21:50:52 -0800

    ehworeih

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, redirect, url_for
+#from pymongo import MongoClient
+#import pprint
 
 app = Flask(__name__)
 app.debug = True
+#db = client['test-database']
+def createPost(math,english,science):
+	post = {
+
+	 "Math": math, 
+	 "English": english,
+	 "Science": science
+	}
+	return post 
+    
+
+#client = MongoClient('mongodb://localhost:27017/')
+
 
 @app.route('/', methods=['GET'])
 def root():
@@ -10,7 +25,9 @@ def root():
 
 @app.route('/home', methods=['GET'])
 def home():
-    return render_template("home.html")
+	links = [{"name":"Freniel","link":"/students/Freniel"},{"name":"Brianna","link":"/students/Brianna"},{"name":"Marty","link":"/students/Marty"},{"name":"Nathan","link":"/students/Nathan"}]
+	return render_template("home.html",items=links)
+	
 
 @app.route('/graph', methods=['GET'])
 def graph():
@@ -19,8 +36,21 @@ def graph():
 
 @app.route('/students/<student>', methods=['GET'])
 def student(student):
-    return render_template("student-profile.html",student = student)
+	subject_grades = createPost(23,32,32)
+	links = [{"name":"Math","link":"/students/"+student + "/Math"},{"name":"English","link":"/students/"+student + "/English"},{"name":"Science","link":"/students/"+student + "/Science"}]
+	return render_template("student-profile.html",student= student, items= links,progress = subject_grades)
 
+@app.route('/students/<student>/<course>', methods=['GET'])
+def studentCourses(student,course):
+	subject_grades = createPost(23,32,32)
+	links = [{"name":"Adding","link":"/students/"+student + "/"+ course + "/Adding"},{"name":"Subtracting","link":"/students/"+student + "/"+ course + "/Subtracting"},{"name":"Division","link":"/students/"+student + "/"+ course + "/Division"},{"name":"Multiplication","link":"/students/"+student + "/"+ course + "/Multiplication"}]
+	return render_template("student-profile.html",student= student, items= links,progress = subject_grades)
+
+
+
+@app.route('/students/<student>/<course>/<subject>', methods=['GET'])
+def studentSubjects(student,course,subject):
+	return "{} {} {} ".format(student,course,subject)
 
 if __name__ == '__main__':
     app.run()
