134ca8d0330bbeb3fa23c0b34560dca54bb01cfe
Freniel Zabala <frenielzabala@gmail.com>
2018-01-20 17:38:55 -0800

    add example

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/', methods=['GET'])
-def hello_world():
+def root():
      return redirect(url_for('home'))
 
 
@@ -11,11 +12,14 @@ def hello_world():
 def home():
     return render_template("home.html")
 
+@app.route('/graph', methods=['GET'])
+def graph():
+    return render_template("example.html")
 
-@app.route('/students/<students>', methods=['GET'])
-def student():
-    return "User %s" % username
 
+@app.route('/students/<student>', methods=['GET'])
+def student(student):
+    return render_template("student-profile.html",student = student)
 
 
 if __name__ == '__main__':
